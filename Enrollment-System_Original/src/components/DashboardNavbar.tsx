d237db977185112c8a5ef37d53d793daa95dc8cb
Pringoottt <estradaprincr56@gmail.com>
2025-03-12 09:21:48 +0800

    change the name of the dashborads and buttons

--- Enrollment-System_Original/src/components/DashboardNavbar.tsx
+++ Enrollment-System_Original/src/components/DashboardNavbar.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Home, LayoutDashboard, Settings, Users, BarChart } from "lucide-react";
+import { FileSearch, MessageCircle, FileText, CreditCard, ListOrdered } from "lucide-react";
 
 export default function DashboardNavbar() {
-  // Unique IDs for Dashboard1 - Dashboard5
   const dashboard1Id = encodeURIComponent("5L^1W(KN~A0dY*J4C)M+zU@oX&bV%9T3qR_?HgpQtwG#O2mB!6h=fxE");
   const dashboard2Id = encodeURIComponent("yK7&*mQzW^B1p@L!g5J#Xv92hC$TdR4N8F(MoZ%qYEwD6xAU)0s!VktG+3bHj?PnM=cf");
   const dashboard3Id = encodeURIComponent("LCyFu=Y?-G?f?PL[?o4f[Nf4Cv*L{kCGWlW9nI3g$:U=ksl}pB;=KzB=`R(l#F:Q7MXe8DZ!GkK!mc");
@@ -16,28 +15,34 @@ export default function DashboardNavbar() {
         <h1 className="text-2xl font-bold">Dashboard</h1>
         <ul className="flex space-x-6">
           <li className="flex items-center space-x-2">
-            <Home className="w-5 h-5" />
+            <FileSearch className="w-5 h-5" />
             <Link to={`/dashboard/${dashboard1Id}`} className="hover:text-gray-300">
-              Dashboard1
+              For Interest Test
             </Link>
           </li>
           <li className="flex items-center space-x-2">
-            <LayoutDashboard className="w-5 h-5" />
+            <MessageCircle className="w-5 h-5" />
             <Link to={`/dashboard2/${dashboard2Id}`} className="hover:text-gray-300">
-              Dashboard2
+              For Interview
             </Link>
           </li>
           <li className="flex items-center space-x-2">
-            <Users className="w-5 h-5" />
-            <Link to={`/dashboard3/${dashboard3Id}`} className="hover:text-gray-300">Dashboard3</Link>
+            <FileText className="w-5 h-5" />
+            <Link to={`/dashboard3/${dashboard3Id}`} className="hover:text-gray-300">
+              For Documents Review
+            </Link>
           </li>
           <li className="flex items-center space-x-2">
-            <BarChart className="w-5 h-5" />
-            <Link to={`/dashboard4/${dashboard4id}`} className="hover:text-gray-300">Dashboard4</Link>
+            <CreditCard className="w-5 h-5" />
+            <Link to={`/dashboard4/${dashboard4id}`} className="hover:text-gray-300">
+              For Payment
+            </Link>
           </li>
           <li className="flex items-center space-x-2">
-            <Settings className="w-5 h-5" />
-            <Link to={`/dashboard5/${dashboard5id}`} className="hover:text-gray-300">Dashboard5</Link>
+            <ListOrdered className="w-5 h-5" />
+            <Link to={`/dashboard5/${dashboard5id}`} className="hover:text-gray-300">
+              Sectioning
+            </Link>
           </li>
         </ul>
       </div>
